e8428b8758dd177f114af857adee931ab87a3de0
LiahWoody <106924400+LiahWoody@users.noreply.github.com>
2025-08-29 14:08:15 +0000

    Add intro image

--- src/intro/Intro.js
+++ src/intro/Intro.js
@@ -1,13 +1,16 @@
+
 import React from "react";
 import "./Intro.css";
+import birthdayImage from "./intro_birthday.jpg";
 
 function Intro() {
   return (
-    <div className="intro_container">
-      <h2>YUAN ROCKFEST</h2>
-      <p>모바일 청첩장에 오신 것을 환영합니다.</p>
-      <div className="image_left">홍길동 · 김유진</div>
-      <div className="image_right">Made by LiahWoody</div>
+    <div className="intro_fullscreen">
+      <img
+        src={birthdayImage}
+        alt="Happy 1st Birthday"
+        className="intro_fullscreen_img"
+      />
     </div>
   );
 }

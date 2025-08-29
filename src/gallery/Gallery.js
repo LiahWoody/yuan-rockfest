fb11691a0f2343713b13868c3a2f5b2b3b4a79a5
Jonguk Lee <ljay.mori@gmail.com>
2025-08-29 23:58:57 +0900

    Update message

--- src/gallery/Gallery.js
+++ src/gallery/Gallery.js
@@ -1,34 +1,9 @@
-import React from "react";
 import "./Gallery.css";
 
 function Gallery() {
   return (
     <div className="gallery_container">
-      <div className="first_container">
-        <div className="first_text1">OUR LOVE</div>
-        <div className="first_text2">우리의 사랑</div>
-      </div>
-      <div className="second_container">
-        <div className="second_text">you mean everything</div>
-      </div>
-      <div className="third_container">
-        <div className="third_text">You love me,<br/>so I can breath</div>
-      </div>
-      <div className="fourth_container">
-        <div className="fourth_text">I Love You More Than Yesterday</div>
-      </div>
-      <div className="fifth_container">
-        <div>Always together</div>
-      </div>
-      <div className="sixth_container">
-        <div className="sixth_text">You are always<br/>gonna be my love</div>
-      </div>
-      <div className="eighth_container">
-        <div>Thank you for coming</div>
-      </div>
-      <div className="nineth_container">
-        <div className="nineth_text">Thank you</div>
-      </div>
+      
     </div>
   );
 }
